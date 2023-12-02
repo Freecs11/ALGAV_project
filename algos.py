2703045d2608bf302f85e672d8def255c26f70b8
rachidB <bouhmad03@gmail.com>
2023-12-02 12:11:55 +0100

    modif du test de l'union tas min

--- algos.py
+++ algos.py
@@ -547,30 +547,32 @@ list_of_sizes = [1000, 5000 ,10000, 20000, 50000, 80000, 120000 ,200000]
 # plt.savefig("experiments/temps_d_ajout_tree.png")
 
 
-# def moyenne_temps_union_table(list_of_sizes):
-#     list_of_times = []
-#     for size in list_of_sizes:
-#         list_of_times_for_size = []
-#         for i in range(1,5 , 2):
-#             list_of_values1 = st.treat_from_file("cles_alea/jeu_"+str(i)+"_nb_cles_"+str(size)+".txt")
-#             list_of_values2 = st.treat_from_file("cles_alea/jeu_"+str(i+1)+"_nb_cles_"+str(size)+".txt")
-#             heap1 = build_heap_table(list_of_values1)
-#             heap2 = build_heap_table(list_of_values2)
-#             start_time = time.time()
-#             Union(heap1, heap2)
-#             end_time = time.time()
-#             list_of_times_for_size.append(end_time - start_time)
-#         list_of_times.append(np.mean(list_of_times_for_size))
-#     return list_of_times
+# le test d'union fait les tests suivant :
+# pour une taille de liste donnée,exemple 1000, fait jeu1 union jeu2, jeu2 union jeu3, jeu3 union jeu4 et jeu4 union jeu5
+def moyenne_temps_union_table(list_of_sizes):
+    list_of_times = []
+    for size in list_of_sizes:
+        list_of_times_for_size = []
+        for i in range(1,5 , 1):
+            list_of_values1 = st.treat_from_file("cles_alea/jeu_"+str(i)+"_nb_cles_"+str(size)+".txt")
+            list_of_values2 = st.treat_from_file("cles_alea/jeu_"+str(i+1)+"_nb_cles_"+str(size)+".txt")
+            heap1 = build_heap_table(list_of_values1)
+            heap2 = build_heap_table(list_of_values2)
+            start_time = time.time()
+            Union(heap1, heap2)
+            end_time = time.time()
+            list_of_times_for_size.append(end_time - start_time)
+        list_of_times.append(np.mean(list_of_times_for_size))
+    return list_of_times
 
-# list_of_times = moyenne_temps_union_table(list_of_sizes)
-# plt.clf()
-# plt.plot(list_of_sizes, list_of_times)
-# plt.xlabel("taille de la lste")
-# plt.ylabel("temps d'union")
-# plt.title("temps d'union en fonction de la taille de la liste")
-# # plt.show()
-# plt.savefig("experiments/temps_d_union_table.png")
+list_of_times = moyenne_temps_union_table(list_of_sizes)
+plt.clf()
+plt.plot(list_of_sizes, list_of_times)
+plt.xlabel("taille de la lste")
+plt.ylabel("temps d'union")
+plt.title("temps d'union en fonction de la taille de la liste")
+# plt.show()
+plt.savefig("experiments/temps_d_union_table.png")
 
 
 
