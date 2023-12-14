505df00f4dc470cd633cfe2fc4c402cb9842e014
rachidB <bouhmad03@gmail.com>
2023-12-14 22:12:07 +0100

    changé le test union file binomial , faut testé avec 1000 et plus

--- BinomialQueueExperiment.py
+++ BinomialQueueExperiment.py
@@ -27,14 +27,15 @@ def moyenne_temps_union(list_of_sizes):
         for i in range(1,5 , 1):
             list_of_values1 = st.treat_from_file("cles_alea/jeu_"+str(i)+"_nb_cles_"+str(size)+".txt")
             list_of_values2 = st.treat_from_file("cles_alea/jeu_"+str(i+1)+"_nb_cles_"+str(size)+".txt")
-            binomialQueue1 = BinomialQueue()
-            binomialQueue2 = BinomialQueue()
-            binomialQueue1.construction(list_of_values1)
-            binomialQueue2.construction(list_of_values2)
-            start_time = time.time()
-            binomialQueue1.union(binomialQueue2)
-            end_time = time.time()
-            list_of_times_for_size.append(end_time - start_time)
+            for j in range(10):
+                binomialQueue1 = BinomialQueue()
+                binomialQueue2 = BinomialQueue()
+                binomialQueue1.construction(list_of_values1)
+                binomialQueue2.construction(list_of_values2)
+                start_time = time.time()
+                binomialQueue1.union(binomialQueue2)
+                end_time = time.time()
+                list_of_times_for_size.append(end_time - start_time)
         list_of_times.append(np.mean(list_of_times_for_size))
     return list_of_times
 
@@ -56,14 +57,14 @@ def representation_graphique(abscisse, ordonnee, nom_fichier, xlabe='', ylabe=''
 if __name__ == '__main__':
     
     list_of_sizes = [1000, 5000 ,10000, 20000, 50000, 80000, 120000 ,200000]
-    list_moyenne_temps_construction = moyenne_temps_construction(list_of_sizes)
-    print (list_moyenne_temps_construction)
-    xlabel = "liste de taille n"
-    ylabel = "temps de construction en secondes"
-    titre = "Temps d'execution de la construction d'une fil binomial \n en fonction de la taille de la liste"
-    label = "construction"
-    nom_fichier = "binomialQueue_construction_temps.png"
-    representation_graphique(list_of_sizes, list_moyenne_temps_construction, nom_fichier, xlabel, ylabel, titre, label)    
+    # list_moyenne_temps_construction = moyenne_temps_construction(list_of_sizes)
+    # print (list_moyenne_temps_construction)
+    # xlabel = "liste de taille n"
+    # ylabel = "temps de construction en secondes"
+    # titre = "Temps d'execution de la construction d'une fil binomial \n en fonction de la taille de la liste"
+    # label = "construction"
+    # nom_fichier = "binomialQueue_construction_temps.png"
+    # representation_graphique(list_of_sizes, list_moyenne_temps_construction, nom_fichier, xlabel, ylabel, titre, label)    
     
     list_moyenne_temps_union = moyenne_temps_union(list_of_sizes)
     print (list_moyenne_temps_union)
